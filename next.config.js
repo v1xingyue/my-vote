247482e74fe359287aa5337e9e0a6924a99b8dc5
v1xingyue <qixingyue@gmail.com>
2025-02-20 17:24:38 +0800

    add pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+}
+
+module.exports = nextConfig 
